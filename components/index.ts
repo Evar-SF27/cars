f8b2687e38c2d80b3269ed66bbc5d04c9ea1abe1
Evar-SF27 <evarfomuso29@gmail.com>
2023-07-05 21:57:37 +0100

    added navbar

--- components/index.ts
+++ components/index.ts
@@ -1,2 +1,4 @@
 export { default as Hero } from "./hero"
 export { default as CustomButton } from "./customButton"
+export { default as NavBar } from "./navBar"
+export { default as Footer } from "./footer"
