0fc137f9b067a90f760619e0d3892e82e39c5abc
Evar-SF27 <evarfomuso29@gmail.com>
2023-07-14 13:58:27 +0100

    added car image api

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { CarProps } from "@types"
+
 export async function fectchCars() {
     const headers = {
         'X-RapidAPI-Key': `${process.env.X_KEY}`,
@@ -24,4 +26,17 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
     const rentalRatePerDay = basePricePerDay + mileageRate + ageRate
   
     return rentalRatePerDay.toFixed(0)
-  };
+}
+
+export const generateCarImageurl = (car: CarProps, angle: string) => {
+    const url = new URL('https://cdn.imagin.studio/getimage')
+    const { make, year, model } = car
+    url.searchParams.append('customer', 'hrjavascript-mastery')
+    url.searchParams.append('make', make)
+    url.searchParams.append('modelFamily', model.split(" ")[0])
+    url.searchParams.append('zoomType', 'fullscreen')
+    url.searchParams.append('modelYear', `${year}`)
+    url.searchParams.append('angle', `${angle}`)
+
+    return `${url}`
+}
